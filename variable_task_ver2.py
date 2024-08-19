f226d6965b1af7eccedddb7cc921b36cb184e365
Oleg <ov.orlovskiy@mail.ru>
2024-08-19 12:38:00 +0300

    Add files via upload

---
+++ variable_task_ver2.py
@@ -0,0 +1,6 @@
+quantity_task = 12
+quantity_hour = 1.5
+name = 'Python'
+task1_time = (quantity_hour/quantity_task)
+name_2 = 'Курс'
+print(name_2, name, 'всего задач', quantity_task, 'затрачено часов', quantity_hour, 'среднее время выполнения', task1_time, 'минут')
